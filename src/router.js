14ec7e85c35c3ecf1478c0268f85cefbdb63dca2
Antonio Pires <tonan@outlook.com.br>
2022-03-18 17:16:36 -0300

    217. Navegação Hash vs History

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router'; //createWebHashHistory
 import Home from '@/views/Home.vue';
 import Login from '@/views/Login.vue';
 
@@ -14,7 +14,8 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    //history: createWebHashHistory(), //seta o modo hash, na navegação
+    history: createWebHistory(), //seta o modo History, na navegação
     routes: routes
     //routes   -- outra forma, somente assim pois tem o mesmo nome (chave e valor)
 });
