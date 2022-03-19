105ac06533049851820a2059d2c0b64f4788f0f1
Antonio Pires <tonan@outlook.com.br>
2022-03-19 15:42:24 -0300

    233. Leads - Listando registros

--- src/components/vendas/Leads.vue
+++ src/components/vendas/Leads.vue
@@ -1,7 +1,34 @@
 <template>
     <div>
         <h5>Leads</h5>
-        {{ dados }}
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">NOME</th>
+                    <th scope="col">TELEFONE</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="d in dados" :key="d.id">
+                    <td>{{ d.id }}</td>
+                    <td>{{ d.nome }}</td>
+                    <td>{{ d.telefone }}</td>
+                    <td>
+                        <router-link class="btn btn-sm btn-primary" to="/home/vendas/leads">
+                            <i class="bi bi-pencil-square" style="cursor: pointer"></i>
+                        </router-link>
+                    </td>
+                </tr>
+                <!-- OU v-for DA FORMA ABAIXO --
+                <tr v-for="d in dados" :key="d">
+                    <td v-for="value in d" :key="value.id">{{value}}</td>
+                </tr>
+                -->
+            </tbody>
+        </table>
+        <!--{{ dados }}-->
     </div>
 </template>
 
