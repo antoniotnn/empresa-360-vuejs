3a58a7f97c6df9f7b219d6257d835d7495d40498
Antonio Pires <tonan@outlook.com.br>
2022-03-19 16:30:44 -0300

    236. Lead - Exibindo dados

--- src/components/vendas/Lead.vue
+++ src/components/vendas/Lead.vue
@@ -1,13 +1,53 @@
 <template>
-    <h5>Detalhes da Lead {{ $route.params.id }}</h5>
+    <div>
+        <h5>{{ dados.id }} - {{ dados.nome }}</h5>
+        <!--{{ dados }}-->
+        <div class="mb-3 row">
+            <label class="col-sm-2 col-form-label">ID</label>
+            <div class="col-sm-10">
+                <input type="text" readonly class="form-control-plaintext" :value="dados.id">
+            </div>
+        </div>
+
+        <div class="mb-3 row">
+            <label class="col-sm-2 col-form-label">ID</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" :value="dados.nome">
+            </div>
+        </div>
+
+        <div class="mb-3 row">
+            <label class="col-sm-2 col-form-label">ID</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" :value="dados.telefone">
+            </div>
+        </div>
+
+        <div class="col-auto">
+            <button type="button" class="btn btn-primary">Atualizar</button>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
     name: 'Lead',
-    
-    /*created() {
-        console.log(this.$route.params);
-    }*/
+    data: () => ({
+        dados: null
+    }),
+    methods: {
+        getDadosApi() {
+            fetch(`http://localhost:3000/leads/${this.$route.params.id}`)
+                .then(response => response.json())
+                .then(response => {
+                    //console.log(response)
+                    this.dados = response;
+                })
+        }
+    },
+    created() {
+        //console.log(this.$route.params);
+        this.getDadosApi();
+    }
 }
 </script>
