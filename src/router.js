6fc20bf6fc14ffdc83268728751c15e1068fa481
Antonio Pires <tonan@outlook.com.br>
2022-03-19 11:15:56 -0300

    225. Rotas alinhadas (rotas filhas) parte 1

--- src/router.js
+++ src/router.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'; //createWebHashHistory
+import Vendas from '@/components/vendas/Vendas.vue';
+import Servicos from '@/components/servicos/Servicos.vue';
 import Home from '@/views/Home.vue';
 import Login from '@/views/Login.vue';
 import Site from '@/views/Site.vue';
@@ -10,7 +12,11 @@ const routes = [
     },
     {
         path: '/home', //localhost:8080/home
-        component: Home
+        component: Home,
+        children: [
+            { path: 'vendas', component: Vendas }, //localhost:8080/home/vendas (obs, na declaração, não iniciar com / pois se fizer isso o vue vai entender que partirá da raiz, ou seja localhost:8080/ . Sem colocar a barra, já entende-se q é uma rota filha, de dentro de /home)
+            { path: 'servicos', component: Servicos }
+        ]
     },
     {
         path: '/login', //localhost:8080/login
