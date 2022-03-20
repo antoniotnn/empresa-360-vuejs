e4985057bbb7d509118c01cf1e3ef0a7420d7151
Antonio Pires <tonan@outlook.com.br>
2022-03-20 10:07:12 -0300

    243. Servico - Apresentando detalhes do serviço

---
+++ src/components/servicos/Servico.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card">
+        <div class="card-header bg-primary text-white">{{ dados.servico }}</div>
+        <div class="card-body">
+            <p class="card-text">{{ dados.descricao }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import ApiMixin from '@/mixins/ApiMixin.js';
+
+export default {
+    name: 'Servico',
+    mixins: [ApiMixin],
+    created() {
+        //console.log(this.$route.params);
+        this.getDadosApi(`http://localhost:3000/servicos/${this.$route.params.id}`);
+    }
+}
+</script>
--- src/router.js
+++ src/router.js
@@ -7,6 +7,7 @@ import Leads from '@/components/vendas/Leads.vue';
 import Lead from '@/components/vendas/Lead.vue';
 import Login from '@/views/Login.vue';
 import Servicos from '@/components/servicos/Servicos.vue';
+import Servico from '@/components/servicos/Servico.vue';
 import Site from '@/views/Site.vue';
 import Vendas from '@/components/vendas/Vendas.vue';
 import VendasPadrao from '@/components/vendas/VendasPadrao.vue';
@@ -28,7 +29,11 @@ const routes = [
                     { path: '', component: VendasPadrao } //localhost:8080/home/vendas/  (componente padrao na rota raiz, com path em branco)
                 ] 
             }, //localhost:8080/home/vendas (obs, na declaração, não iniciar com / pois se fizer isso o vue vai entender que partirá da raiz, ou seja localhost:8080/ . Sem colocar a barra, já entende-se q é uma rota filha, de dentro de /home)
-            { path: 'servicos', component: Servicos, name: 'servicos' }, //localhost:8080/home/servicos
+            { path: 'servicos', component: Servicos, name: 'servicos', children: 
+                [
+                    { path: ':id', component: Servico, name: 'servico' } //localhost:8080/home/servicos/5
+                ] 
+            }, //localhost:8080/home/servicos
             { path: 'dashboard', component: Dashboard } //localhost:8080/home/dashboard
         ]
     },
