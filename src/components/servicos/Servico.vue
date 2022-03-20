f1d5c0cedabbbd826e704b10ae796bb763d7e922
Antonio Pires <tonan@outlook.com.br>
2022-03-20 10:42:03 -0300

    246. Reagindo a alterações na url com beforeRouteUpdate

--- src/components/servicos/Servico.vue
+++ src/components/servicos/Servico.vue
@@ -15,15 +15,27 @@ export default {
     mixins: [ApiMixin],
     created() {
         //console.log(this.$route.params);
-        console.log('Componente Serviço Criado');
+        //console.log('Componente Serviço Criado');
         this.getDadosApi(`http://localhost:3000/servicos/${this.$route.params.id}`);
     },
-    watch: {
+    beforeRouteUpdate(to, from, next) { //metodo para fazer tratativa antes de atualizar uma rota. Alternativa para a abordagem watch, comentada abaixo.
+        //to = $route para onde estamos indo
+        //from = $route de onde estamos vindo
+        //next faz com que o fluxo de navegação siga em frente
+
+        //console.log(to.params.id);
+        if(to.params.id != undefined) this.getDadosApi(`http://localhost:3000/servicos/${to.params.id}`);
+        //console.log(from.params.id);
+        next();
+
+    }
+    /*watch: {
         //$route(novovalor, valorAntigo) { //convenção diz para usar os nomes to, from (para onde foi na rota, e veio de onde; to = novoValor, from = valorAntigo)
         $route(to) { 
             //console.log(to.params);
             if(to.params.id != undefined) this.getDadosApi(`http://localhost:3000/servicos/${to.params.id}`);
         }
-    }
+    }*/
+
 }
 </script>
