61cc78762f6f56417e7dbef9a050c0e5ce8fd471
Antonio Pires <tonan@outlook.com.br>
2022-03-20 17:22:21 -0300

    252. Contratos - Listando registros

--- src/components/vendas/Contratos.vue
+++ src/components/vendas/Contratos.vue
@@ -1,3 +1,37 @@
 <template>
-    <h5>Contratos</h5>
+    <div>
+        <h5>Contratos</h5>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">LEAD</th>
+                    <th scope="col">SERVIÇO</th>
+                    <th scope="col">DATA INÍCIO</th>
+                    <th scope="col">DATA FIM</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="d in dados" :key="d.id">
+                    <td>{{ d.id }}</td>
+                    <td>{{ d.leadId }}</td>
+                    <td>{{ d.servicoId }}</td>
+                    <td>{{ d.data_inicio }}</td>
+                    <td>{{ d.data_fim }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
+
+<script>
+import ApiMixin from '@/mixins/ApiMixin.js';
+
+export default {
+    name: 'Contratos',
+    mixins: [ApiMixin],
+    created() {
+        this.getDadosApi('http://localhost:3000/contratos');
+    }
+}
+</script>
